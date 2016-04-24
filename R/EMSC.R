c8754fb77415ca2b61f3b71145e4e1b0b8cc8b37
Kristian Hovde Liland <kristian.liland@nmbu.no>
2016-04-24 22:04:59 +0200

    Extended functionality
    Added print, summary, plot, predict, orthogonalVectors and replicate
    correction.

--- R/EMSC.R
+++ R/EMSC.R
@@ -17,9 +17,11 @@
 #'  \item{\code{corrected}:}{ \code{matrix} of corrected spectra.}
 #'  \item{\code{parameters}:}{ \code{matrix} of fitted parameter values.}
 #'  \item{\code{model}:}{ object containing input all input parameters.}
+#'  \item{\code{X}:}{ original data.}
 #' }
 #'
-#' @seealso \code{\link{EMSC_model}} \code{\link{predict.EMSC}}
+#' @seealso \code{\link{EMSC_model}} \code{\link{predict.EMSC}} \code{\link{plot.EMSC}}
+#' 
 #' @references H. Martens, E. Stark, Extended multiplicative signal correction and spectral
 #'  interference subtraction: new preprocessing methods for near infrared spectroscopy.
 #'  J Pharm Biomed Anal. 1991; 9(8):625-35.
@@ -29,7 +31,8 @@
 #' Raman      <- milk$Raman[, 850:3300]
 #' EMSC.basic <- EMSC(Raman)
 #' EMSC.poly6 <- EMSC(Raman, degree = 6)
-#' EMSC.ref   <- EMSC(Raman, degree = 6, reference = Raman[30, ])
+#' EMSC.rep   <- EMSC(Raman, degree = 6, reference = Raman[30, ],
+#'                    replicates = milk$replicates)
 #'
 #' \dontrun{
 #' old.par  <- par(mfrow = c(2,2), mar = c(4,4,1,1))
@@ -40,8 +43,9 @@
 #'         ylab = 'Relative intensity', xlab = 'Corrected (basic)')
 #' matplot(colnames(Raman), t(EMSC.poly6$corrected), type = 'l', xlim = xlim,
 #'         ylab = 'Relative intensity', xlab = 'Corrected (6th degree polynomial)')
-#' matplot(colnames(Raman), t(EMSC.ref$corrected),   type = 'l', xlim = xlim,
-#'         ylab = 'Relative intensity', xlab = 'Corrected (reference = spec. #30)')
+#' matplot(colnames(Raman), t(EMSC.rep$corrected),   type = 'l', xlim = xlim,
+#'         ylab = 'Relative intensity', 
+#'         xlab = 'Corrected (reference = spec. #30, replicate correction (90%))')
 #' par(old.par)
 #' }
 #'
@@ -50,6 +54,10 @@
 EMSC <- function(X, model = NULL, ...){
   n <- dim(X)[1]
 
+  # Strip X
+  X <- as.matrix(X)
+  X <- unclass(X)
+  
   # Make EMSC model if not supplied
   if(is.null(model)){
     mf <- match.call(expand.dots = TRUE)
@@ -60,7 +68,7 @@ EMSC <- function(X, model = NULL, ...){
   }
 
   # Extract from model object
-  terms <- model$sizes[2] + 2 + model$sizes[3] # Polynomial + interferents
+  terms <- model$sizes[2] + 2 + model$sizes[3] + model$sizes[5] # Polynomial + interferents + replicate model
 
   # Apply weights (if included)
   if(!is.null(model$weights)){
@@ -76,7 +84,7 @@ EMSC <- function(X, model = NULL, ...){
 
   # Perform parameter estimation
   if(any(PP)){
-    Parameters0  <- mldivide(t(modelw), t(Xw[!PP,]))
+    Parameters0  <- mldivide(t(modelw), t(Xw[!PP,]), pinv = FALSE)
 
     k <- 2:terms
     Corrected0 <- Xw[!PP,] - crossprod(Parameters0[k,, drop = FALSE], modelw[k,, drop = FALSE])
@@ -87,7 +95,7 @@ EMSC <- function(X, model = NULL, ...){
     parameters[,!PP] <- Parameters0
 
   } else {
-    Parameters0  <- mldivide(t(modelw), t(Xw))
+    Parameters0  <- mldivide(t(modelw), t(Xw), pinv = FALSE)
 
     k <- 2:terms
     Corrected0 <- Xw - crossprod(Parameters0[k,, drop = FALSE], modelw[k,, drop = FALSE])
@@ -98,7 +106,7 @@ EMSC <- function(X, model = NULL, ...){
   }
 
   # Return
-  object <- list(corrected = corrected, parameters = parameters, model = model)
+  object <- list(corrected = corrected, parameters = parameters, model = model, X = X)
   class(object) <- c("EMSC", "list")
   object
 }
@@ -108,21 +116,24 @@ EMSC <- function(X, model = NULL, ...){
 #'
 #' Sets up an EMSC model to be applied to one or more set of spectra.
 #'
-#' @param x \code{numeric} vector containing abcissas of spectra to be corrected, reference spectrum
-#' with/without names or matrix to be corrected.
+#' @param x \code{numeric} vector containing abcissas of spectra to be corrected or matrix to be 
+#' corrected with/without names colnames.
 #' @param reference \code{numeric} vector containing the reference spectrum.
 #' @param degree \code{integer} giving the polynomial degree of the baseline; 0 or higher, default = 2.
 #' @param interferent \code{numeric} vector containing a spectral component to remove.
 #' @param constituent \code{numeric} vector containing a spectral component to include.
 #' @param weights \code{numeric} vector of abcissas weights.
-#' @param rep_corr proportion of variance or number of subspace components in replicate space (not implemented yet).
+#' @param replicates optional \code{vector} which identifies replicates. Default = NULL, 
+#' meaning no replicate correctio will be performed.
+#' @param rep_corr proportion of variance or number of subspace components in replicate space (default = 0.9).
 #'
 #' @return An EMSC model is returned containing all parameters.
 #'
-#' @seealso \code{\link{EMSC}} \code{\link{predict.EMSC}}
+#' @seealso \code{\link{EMSC}} \code{\link{predict.EMSC}} \code{\link{plot.EMSC}}
 #' @export
 EMSC_model <- function(x, reference = NA, degree = 2,
-                       interferent = NULL, constituent = NULL, weights = NULL, rep_corr = NULL){
+                       interferent = NULL, constituent = NULL, weights = NULL,
+                       replicates = NULL, rep_corr = 0.9){
 
   # Handle abcissas
   if(is.data.frame(x))
@@ -169,7 +180,13 @@ EMSC_model <- function(x, reference = NA, degree = 2,
       model[3,] <- M*(Start-abcissas)-1
     }
   }
-
+  
+  # Orthogonalize model spectra (except reference) to avoid overlap
+  if(degree > 2){
+    model[-1, ] <- t(qr.Q(qr(t(model[-1, , drop = FALSE])))[,1:(degree+1)])
+    model[ 2, ] <- abs(model[2, ])
+  }
+  
   # Add interferents and constituents
   model <- rbind(model, interferent, constituent)
   if(!is.null(interferent)){
@@ -179,44 +196,97 @@ EMSC_model <- function(x, reference = NA, degree = 2,
     mod_names <- c(mod_names, paste("Constituent", 1:n.c))
   }
 
-  # Handle replicate correction
-  # (not implemented yet)
-
   # Add dimnames
   dimnames(model) <- list(term = mod_names, abcissas = abcissas)
-
-  # Return
-  list(model = model, abcissas = abcissas, sizes = c(p, degree, n.i, n.c), weights = weights)
+  
+  # Handle replicate correction
+  if(!is.null(replicates) && length(replicates) > 1){
+    M <- calculateReplicateModel(x, replicates, rep_corr,
+                                     list(model = model, 
+                                          abcissas = abcissas, 
+                                          sizes = c(p, degree, n.i, n.c, 0), 
+                                          weights = weights))
+    return(M)
+  } else { # ... or return without
+    return(list(model = model, 
+                abcissas = abcissas, 
+                sizes = c(p, degree, n.i, n.c, 0), 
+                weights = weights))
+  }
 }
 
 
-#' Predict Method for EMSC
-#' 
-#' Prediction for \code{EMSC} ojects. Corrections are calculated for the new
-#' \code{matrix} based on the EMSC model used in the input object.
-#' 
-#' @param object An object fitted by the \code{EMSC} function.
-#' @param newdata A \code{matrix} or object convertable to a matrix containing observations as rows.
-#' 
-#' @seealso \code{\link{EMSC}} \code{\link{EMSC_model}}
-#' 
-#' @examples
-#' data(milk)
-#' Raman.cal <- milk$Raman[  1:90,  850:3300]
-#' Raman.val <- milk$Raman[-(1:90), 850:3300]
-#' EMSC.cal  <- EMSC(Raman.cal)
-#' EMSC.val  <- predict(EMSC.cal, Raman.val)
-#' identical(EMSC.cal$model, EMSC.val$model) # Same model, reference spectrum, etc.
-#' 
-#' matplot(t(EMSC.cal$corrected), type = 'l', col = 'black', lty = 1, ylab = 'Intensity')
-#' matplot(t(EMSC.val$corrected), type = 'l', col = 'red', lty = 2, add = TRUE)
-#' legend('topleft', legend = c('Calibration','Validation'), lty = 1:2, col = 1:2)
-#' 
-#' @export
-predict.EMSC <- function(object, newdata = NULL){
-  if(is.null(newdata))
-    return(object)
+# Not exported calculation of replicate vectors
+calculateReplicateModel <- function(x, replicates, rep_corr, model){
+  # Remove singletons
+  n_reps <- table(replicates)
+  if(any(n_reps == 1)){
+    out <- numeric(0)
+    for(i in unique(replicates)){
+      if(sum(ind <- replicates == i) == 1){
+        out <- c(out, which(ind))
+      }
+    }
+    x_rep <- x[-out, , drop = FALSE]
+    replicates <- replicates[-out]
+  } else {
+    x_rep <- x
+  }
   
-  newdata <- unclass(as.matrix(newdata))
-  EMSC(newdata, object$model)
+  # Save reference
+  reference <- model$model[1,]
+  
+  # Loop over replicate sets
+  uniqueReplicates <- unique(replicates)
+  nreps    <- length(uniqueReplicates)
+  meanCorr <- matrix(0, nreps, model$sizes[1])
+  resCorr  <- matrix(0, 0, model$sizes[1])
+  j <- 0
+  for(i in uniqueReplicates){
+    # Correct current replicate set
+    reps <- x[replicates == i, ]
+    j <- j+1
+    model$model[1,] <- colMeans(x) 
+    repsCorrected <- EMSC(reps, model = model)
+    
+    # Compute replicate means and corrected residuals
+    meanCorr[j,] <- colMeans(repsCorrected$corrected)
+    resCorr      <- rbind(resCorr, repsCorrected$corrected - rep(meanCorr[j,], each = dim(reps)[1]))
+  }
+  
+  # Weight before PCA
+  if(!is.null(model$weights)){
+    resCorr <- resCorr * rep(model$weights, each = dim(resCorr)[1])
+  }
+  usv <- svd(resCorr)
+  
+  # Calculate explained variance
+  ExpVar <- usv$d^2 / sum(usv$d^2)
+  ExpVarCum <- cumsum(ExpVar)
+
+  # Select number of components
+  if(rep_corr < 1){
+    rep_corr <- match(TRUE, ExpVarCum >= rep_corr)
+  }
+  
+  # Insert replicate model as interferents
+  rep_vecs <- usv$v[,1:rep_corr, drop = FALSE]
+  colnames(rep_vecs) <- paste("Rep.vec.",1:rep_corr)
+  if(model$sizes[4] == 0){
+    model$model <- rbind(model$model, t(rep_vecs))
+  } else {
+    degInt <- model$sizes[2]+model$sizes[3]+2
+    model$model <- rbind(model$model[1:degInt],
+                         t(rep_vecs), 
+                         model$model[(degInt+1):(degInt+model$sizes[3])])
+  }
+  model$model[1,] <- reference
+  model$sizes[5]  <- rep_corr
+
+  # Save replicate model
+  model$rep <- list(PCA    = usv,
+                    ExpVar = ExpVarCum,
+                    ncomp  = rep_corr,
+                    deviations = resCorr)
+  model
 }
